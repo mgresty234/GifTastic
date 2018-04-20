e922c0ae07ce9f7a96b8f87d12de7ca067982699
Matt G <mgresty223@gmail.com>
2018-04-20 13:07:50 -0400

    GifTastic

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,6 +1,36 @@
-$("button").on("click", function () {
-   
-    var person = $(this).attr("data-person");
+var animalButtons = $("#animalButtons");
+
+$(document).ready(function () {
+
+    var topics = ['cat', 'dog', 'monkey', 'parrot', 'rabbit', 'squirrel'];
+
+
+
+    for (var i = 0; i < topics.length; i++) {
+        makeTopicButton(topics[i]);
+    }
+
+});
+
+function makeTopicButton(topic) {
+    var topicButton = $('<button>');
+    topicButton.addClass('topic-button');
+    topicButton.attr('data-name', topic);
+    topicButton.text(topic);
+
+    animalButtons.append(topicButton);
+}
+
+
+$('#addAnimal').on('click', function () {
+    event.preventDefault();
+    var newTopic = $('#animal-input').val();
+    makeTopicButton(newTopic);
+});
+
+$("#animalButtons").on("click", 'button', function () {
+
+    var person = $(this).attr("data-name");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         person + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -10,9 +40,14 @@ $("button").on("click", function () {
         method: "GET"
     })
         .then(function (response) {
+
+
+            $("#animals").empty();
+
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
+                console.log(results[i]);
 
                 if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
@@ -29,7 +64,7 @@ $("button").on("click", function () {
                     gifDiv.append(p);
                     gifDiv.append(personImage);
 
-                    $("#animalButtons").prepend(gifDiv);
+                    $("#animals").append(gifDiv);
                 }
             }
         });
